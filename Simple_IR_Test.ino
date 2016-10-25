cfe2327f5bbf8370c2d37b5a6d941882f9d12ed8
yangy2 <yangy2@tcnj.edu>
2016-10-25 16:47:37 -0400

    Update Simple_IR_Test.ino

--- Simple_IR_Test.ino
+++ Simple_IR_Test.ino
@@ -1,3 +1,9 @@
+/*
+HOW TO WIRE THE IR SENSOR TO ARDUINO
+-Connect the Red wire to power (3.3v)
+-Connect the Black wire to ground (GND)
+-Connect the 3rd wire to an analog port
+*/
 int x;
 
 void setup()
@@ -9,5 +15,8 @@ void loop()
 {
  x = analogRead(1); //set x = data from analog port A#
  Serial.println(x); //Print the result 'x'
+ if (x > 400){
+     Serial.println(" --> Object is close!");
+ }
  delay(1000); //Delay the output by 1s (1000ms) 
 }
