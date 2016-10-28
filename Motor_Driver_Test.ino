a39e6e03fe14f5843ada62fdc32d3da50dfd114e
yangy2 <yangy2@tcnj.edu>
2016-10-28 17:56:02 -0400

    Create Motor_Driver_Test.ino

---
+++ Motor_Driver_Test.ino
@@ -0,0 +1,30 @@
+int E1 = 6; //Speed 1
+int M1 = 7; //Direction 1
+int E2 = 9; //Speed 2                         
+int M2 = 8; //Direction 2
+
+//OUTPUT HIGH for Forward
+//OUTPUT LOW for Reverse
+
+void setup() 
+{ 
+    pinMode(M1, OUTPUT);   
+    pinMode(M2, OUTPUT); 
+} 
+
+void loop() 
+{ 
+    digitalWrite(M1,HIGH);   
+    digitalWrite(M2,HIGH);       
+    analogWrite(E1, 250);   //PWM Speed Control
+    analogWrite(E2, 250);   //PWM Speed Control
+    delay(2000); 
+    
+    
+    digitalWrite(M1,LOW);   
+    digitalWrite(M2,LOW);       
+    analogWrite(E1, 250);   //PWM Speed Control
+    analogWrite(E2, 250);   //PWM Speed Control
+    delay(2000); 
+    
+}
