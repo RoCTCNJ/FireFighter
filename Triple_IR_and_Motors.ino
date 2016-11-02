5f6d97da5598759675db747239a77b446cb72904
yangy2 <yangy2@tcnj.edu>
2016-11-02 11:52:19 -0400

    Create Triple_IR_and_Motors.ino

---
+++ Triple_IR_and_Motors.ino
@@ -0,0 +1,45 @@
+nt left, right, front;
+
+int E1 = 6;
+int M1 = 7;
+int E2 = 5;                         
+int M2 = 4; 
+
+
+void setup()
+{
+ Serial.begin(9600); 
+    pinMode(M1, OUTPUT);   
+    pinMode(M2, OUTPUT); 
+}
+
+void loop()
+{
+//IR Sensors
+ left = analogRead(1); //set x = data from analog port A#
+ right = analogRead(2);
+ front = analogRead(3);
+ Serial.print(left); 
+ Serial.print(" ");
+ Serial.print(front);
+ Serial.print(" ");
+ Serial.print(right);
+ Serial.println();
+ delay(1000); //Delay the output by 1s (1000ms) 
+ 
+ 
+ //Motors
+    digitalWrite(M1,HIGH);   
+    digitalWrite(M2,HIGH);       
+    analogWrite(E1, 250);   //PWM Speed Control
+    analogWrite(E2, 250);   //PWM Speed Control
+    delay(2000); 
+    
+    
+    digitalWrite(M1,LOW);   
+    digitalWrite(M2,LOW);       
+    analogWrite(E1, 250);   //PWM Speed Control
+    analogWrite(E2, 250);   //PWM Speed Control
+    delay(2000); 
+ 
+}
