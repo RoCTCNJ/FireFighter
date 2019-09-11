8123fc31ae23ddd41c4beaa7e115cbf9f3066523
Garrett Hope <hopeg1@tcnj.edu>
2019-09-10 23:06:46 -0400

    Added flame sensor example

---
+++ 2019Testing/FlameSensor/FlameSensor.ino
@@ -0,0 +1,32 @@
+//Define constants
+const int sensorMin = 0;
+const int sensorMax = 1024;
+
+//Initialize variables/pins
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+//Main code
+void loop() {
+  //Read flame sensor from port A0
+  int sensorReading = analogRead(A0);
+  //Map reading to 1 of 3 values
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 2);
+
+  //Print results
+  switch (range) {
+  case 0:    // A fire closer than 1.5 feet away.
+    Serial.println("** Close Fire **");
+    break;
+  case 1:    // A fire between 1-3 feet away.
+    Serial.println("** Distant Fire **");
+    break;
+  case 2:    // No fire detected.
+    Serial.println("No Fire");
+    break;
+  }
+  //Wait 1000 ms (1s) before repeating loop
+  delay(1000);
+}
